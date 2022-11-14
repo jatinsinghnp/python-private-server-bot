93d8a51d9fa0b1545f171cc36d2e2d0fcf02eca8
jatinsinghnp <js8971105@gmail.com>
2022-11-14 08:31:21 +0545

    fix command

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
+import replicate
 import discord
 from dotenv import load_dotenv
 import os
 from discord.ext import commands
 
-#intents 
+# intents
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-#.env settings 
-import replicate
+# bot prefix intents
+
+client = commands.Bot(
+    command_prefix="!",
+    description="Runs models on Replicate!",
+    intents=intents,
+)
+
+
+# .env settings
+
 load_dotenv()
-TOKEN=os.environ.get("TOKEN")
+TOKEN = os.environ.get("TOKEN")
 
 
-
-#active status
+# active status
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
 
 
-#user message 
-@client.event
+# user message
+@client.listen()
 async def on_message(message):
     if message.author == client.user:
         return
-
     if message.content.startswith(".hello"):
         await message.channel.send(f"hello {message.author}")
+    
     if message.content[0] == "?":
         await message.channel.send(f"hi,{message.author}")
 
 
-#user join 
-@client.event
+# user join
+@client.listen()
 async def on_member_join(member):
     welcome_channel = client.get_channel(1010188442048663625)
     print(f"{member} has join ")
@@ -43,11 +52,13 @@ async def on_member_join(member):
     try:
         await member.send(f"hey{member.display_name} thank you for joining the server ")
     except:
-        await welcome_channel.send(f"{member.mention} I cant dm you ,but thanks for  joining")
+        await welcome_channel.send(
+            f"{member.mention} I cant dm you ,but thanks for  joining"
+        )
 
 
-#user remove
-@client.event
+# user remove
+@client.listen()
 async def on_member_remove(member):
     welcome_channel = client.get_channel(1010188442048663625)
     print(f"{member} has join ")
@@ -56,27 +67,16 @@ async def on_member_remove(member):
         await member.send(f"hey{member.display_name} goobye  ")
     except:
         await welcome_channel.send(f"{member.mention} I can't dm you ,goodbye")
-        
-#bot command         
 
-client = commands.Bot(
-    command_prefix="!",
-    description="Runs models on Replicate!",
-    intents=intents,
-)
+
 
 @client.command()
 async def make(ctx, *, prompt):
     """Generate an image from a text prompt using the stable-diffusion model"""
     msg = await ctx.send(f"“{prompt}”\n> Generating...")
-
     model = replicate.models.get("stability-ai/stable-diffusion")
     image = model.predict(prompt=prompt)[0]
-
     await msg.edit(content=f"“{prompt}”\n{image}")
-
-
-
 
 
 client.run(TOKEN)
