8407b43ac5dd9e404dac7d4cb8533e82f5148d96
jatinsinghnp <js8971105@gmail.com>
2022-11-14 07:35:38 +0545

    ai art generate added

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import discord
 from dotenv import load_dotenv
 import os
+from discord.ext import commands
+
+#intents 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+
+#.env settings 
+import replicate
 load_dotenv()
 TOKEN=os.environ.get("TOKEN")
 
+
+
+#active status
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
 
 
-
+#user message 
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -25,6 +34,7 @@ async def on_message(message):
         await message.channel.send(f"hi,{message.author}")
 
 
+#user join 
 @client.event
 async def on_member_join(member):
     welcome_channel = client.get_channel(1010188442048663625)
@@ -36,7 +46,7 @@ async def on_member_join(member):
         await welcome_channel.send(f"{member.mention} I cant dm you ,but thanks for  joining")
 
 
-
+#user remove
 @client.event
 async def on_member_remove(member):
     welcome_channel = client.get_channel(1010188442048663625)
@@ -46,8 +56,27 @@ async def on_member_remove(member):
         await member.send(f"hey{member.display_name} goobye  ")
     except:
         await welcome_channel.send(f"{member.mention} I can't dm you ,goodbye")
+        
+#bot command         
+
+bot = commands.Bot(
+    command_prefix="!",
+    description="Runs models on Replicate!",
+    intents=intents,
+)
+
+@bot.command()
+async def make(ctx, *, prompt):
+    """Generate an image from a text prompt using the stable-diffusion model"""
+    msg = await ctx.send(f"“{prompt}”\n> Generating...")
+
+    model = replicate.models.get("stability-ai/stable-diffusion")
+    image = model.predict(prompt=prompt)[0]
+
+    await msg.edit(content=f"“{prompt}”\n{image}")
+
+
+
+bot.run(TOKEN)
 
 client.run(TOKEN)
-
-
-
---
+++ memgen.py
@@ -0,0 +1,5 @@
+from PIL import Image
+from io import BytesIO
+import base64 
+import time 
+
